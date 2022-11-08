c9b1e02ed4344d608989cf2e03fc7599317185a4
thomca <catherine.thomsen@wallawalla.edu>
2022-11-07 20:31:45 -0800

    updating scripts

--- Script/install.sh
+++ Script/install.sh
@@ -4,13 +4,17 @@
 # or that the version of python that is run here is the one 
 # with the dependencies 
 
+# if things are going poorly 
+# python -m pip install --upgrade --force-reinstall pip
+
+# ad python to path
 # Using Python 3.11.2
 # for windows
 # py -m pip install --upgrade pip
 # py -m pip install numpy
 # py -m pip install matplotlib
 # py -m pip install tk
-python -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
 pip install numpy
 pip install matplotlib
 pip install tk
