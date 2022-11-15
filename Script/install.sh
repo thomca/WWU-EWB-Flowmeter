f8e06190432eb1140d0fbef0ebbc2e0257a35907
thomca <catherine.thomsen@wallawalla.edu>
2022-11-14 16:52:46 -0800

    adding in example code for making PDF stuff and updating scripts to be seperate for windows and mac

--- Script/install.sh
+++ Script/install.sh
@@ -7,14 +7,18 @@
 # if things are going poorly 
 # python -m pip install --upgrade --force-reinstall pip
 
+# numpy - a complex numbers library
+# matplotlib - for graphing functions
+# tk - for working on GUIs in python
+# reportlab - for creating pdfs if we need it
+
 # ad python to path
 # Using Python 3.11.2
-# for windows
-# py -m pip install --upgrade pip
-# py -m pip install numpy
-# py -m pip install matplotlib
-# py -m pip install tk
+# for windows check other folder
+
 python3 -m pip install --upgrade pip setuptools wheel
 pip install numpy
 pip install matplotlib
 pip install tk
+pip install reportlab
+pip install pdfrw
---
+++ Script/windows_install.sh
@@ -0,0 +1,6 @@
+py -m pip install --upgrade pip
+py -m pip install numpy
+py -m pip install matplotlib
+py -m pip install tk
+py -m pip install reportlab
+py -m pip install pdfrw
